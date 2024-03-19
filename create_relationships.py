966c19ae027443d868da82f7e1e5d52a1d373d21
Benncame2929 <151690591+Benncame2929@users.noreply.github.com>
2024-03-19 09:07:33 -0400

    added loop

--- create_relationships.py
+++ create_relationships.py
@@ -1,3 +1,6 @@
+import sqlite3
+from random import randint, choice
+from faker import Faker
 """
 Description:
  Creates the relationships table in the Social Network database
@@ -18,15 +21,62 @@ def main():
 
 def create_relationships_table():
     """Creates the relationships table in the DB"""
-    # TODO: Function body
-    # Hint: See example code in lab instructions entitled "Add the Relationships Table"
+    
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    # SQL query that creates a table named 'relationships'.
+    create_relationships_tbl_query = """
+    CREATE TABLE IF NOT EXISTS relationships
+    (
+    id INTEGER PRIMARY KEY,
+    person1_id INTEGER NOT NULL,
+    person2_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    FOREIGN KEY (person1_id) REFERENCES people (id),
+    FOREIGN KEY (person2_id) REFERENCES people (id)
+    );
+    """
+    # Execute the SQL query to create the 'relationships' table.
+    cur.execute(create_relationships_tbl_query)
+    con.commit()
+    con.close()
+   
     return
 
 def populate_relationships_table():
     """Adds 100 random relationships to the DB"""
     # TODO: Function body
-    # Hint: See example code in lab instructions entitled "Populate the Relationships Table"
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    # SQL query that inserts a row of data in the relationships table.
+    add_relationship_query = """
+    INSERT INTO relationships
+    (
+    person1_id,
+    person2_id,
+    type,
+    start_date
+    )
+    VALUES (?, ?, ?, ?);
+    
+    
+    """
+
+    person1_id_example = 1
+    person2_id_example = 2
+    type_example = 'friend'
+    start_date_example = '2024-03-19'
+    for _ in range(100):
+    cur.execute(add_relationship_query, (person1_id_example, person2_id_example, type_example, start_date_example))
+
+
+
+    con.commit()
+    con.close()
+
     return 
+
 
 if __name__ == '__main__':
    main()
